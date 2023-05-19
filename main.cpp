2b2ddbdebfc22fc0b53a6c6796ab40ca01ed6ac8
student <student@PC-G-04>
2023-05-19 20:09:38 +0300

    Task done

--- main.cpp
+++ main.cpp
@@ -1,21 +1,41 @@
+
 #include <iostream>
 #include <windows.h>
+
 using namespace std;
 
-int main() {
-
+int main()
+{
+    srand(GetTickCount());
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    
-    int profit = 0;
-    for (int i = 1; i <= 6; i++) {
-        int monthly_profit;
-        cout << "Введіть прибуток за " << i << "-й місяць: ";
-        cin >> monthly_profit;
-        profit += monthly_profit;
+
+    int count = 10;
+    const int SIZE = 1000;
+    int arr_def[SIZE];
+    for (int i = 0; i < count; i++)
+    {
+        arr_def[i] = 20 + rand() % 30;
+        cout << "Елемент " << i + 1 << ": " << arr_def[i] << endl;
     }
 
-    cout << "Загальний прибуток за 6 місяців: " << profit << endl;
+    int min = arr_def[0];
+    int max = arr_def[0];
+
+    for (int i = 1; i < count; i++)
+    {
+        if (min > arr_def[i])
+        {
+            min = arr_def[i];
+        }
+        if (max < arr_def[i])
+        {
+            max = arr_def[i];
+        }
+    }
+
+    cout << "Мінімальне значення: " << min << endl;
+    cout << "Максимальне значення: " << max << endl;
 
     return 0;
 }
