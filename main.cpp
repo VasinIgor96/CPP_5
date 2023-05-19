b03939a1e5633502b38a46abcb6f540651ab7417
student <student@PC-G-04>
2023-05-19 19:09:23 +0300

    Task done

--- main.cpp
+++ main.cpp
@@ -6,16 +6,21 @@ int main() {
 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    
-    int profit = 0;
-    for (int i = 1; i <= 6; i++) {
-        int monthly_profit;
-        cout << "Введіть прибуток за " << i << "-й місяць: ";
-        cin >> monthly_profit;
-        profit += monthly_profit;
+
+        const int SIZE = 5;
+        int arr[SIZE];
+
+        cout << "Введіть елементи масиву:" << endl;
+        for (int i = 0; i < SIZE; i++) {
+            cout << "Елемент " << i + 1 << ": ";
+            cin >> arr[i];
+        }
+
+        cout << "Масив у зворотньому порядку:" << endl;
+        for (int i = SIZE - 1; i >= 0; i--) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        return 0;
     }
-
-    cout << "Загальний прибуток за 6 місяців: " << profit << endl;
-
-    return 0;
-}
