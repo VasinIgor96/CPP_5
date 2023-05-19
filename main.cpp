359738b9ca6304f70d67f4fa36f8c5e15d73e91e
student <student@PC-G-04>
2023-05-19 19:16:45 +0300

    Task done

--- main.cpp
+++ main.cpp
@@ -7,15 +7,21 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     
-    int profit = 0;
-    for (int i = 1; i <= 6; i++) {
-        int monthly_profit;
-        cout << "Введіть прибуток за " << i << "-й місяць: ";
-        cin >> monthly_profit;
-        profit += monthly_profit;
+        const int NUM_SIDES = 5;
+        double sides[NUM_SIDES];
+
+        cout << "Введіть довжини сторін п'ятикутника:" << endl;
+        for (int i = 0; i < NUM_SIDES; i++) {
+            cout << "Сторона " << i + 1 << ": ";
+            cin >> sides[i];
+        }
+
+        double perimeter = 0;
+        for (int i = 0; i < NUM_SIDES; i++) {
+            perimeter += sides[i];
+        }
+
+        cout << "Периметр п'ятикутника: " << perimeter << endl;
+
+        return 0;
     }
-
-    cout << "Загальний прибуток за 6 місяців: " << profit << endl;
-
-    return 0;
-}
